36e0f064d77c7b6d12d4adae7d5adbe4047736db
Igor <igor.kowalczyk@cyfroweszkoly.pl>
2025-05-23 11:02:20 +0200

    TextBox now add to ListBox city

--- TravelPlanner/MainWindow.axaml.cs
+++ TravelPlanner/MainWindow.axaml.cs
@@ -1,11 +1,29 @@
+using System.Collections.ObjectModel;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace TravelPlanner;
 
 public partial class MainWindow : Window
 {
+    private ObservableCollection<string> miasta = new ObservableCollection<string>();
+
     public MainWindow()
     {
         InitializeComponent();
+        miastoLista.ItemsSource = miasta;
+        miastoButton.Click += dodajMiasto;
+    }
+
+    public void dodajMiasto(object? sender, RoutedEventArgs e)
+    {
+        string miasto = miastoDodaj.Text.Trim();
+
+        if (!string.IsNullOrEmpty(miasto))
+        {
+            miasta.Add(miasto);
+            miastoDodaj.Text = "";
+        }
     }
 }
